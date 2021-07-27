617487736757745fcd27f2c3f770a5816d767ce4
Titus . Roman <titus@CIT-SCC123-1524.cs.dixie.edu>
2021-07-27 12:07:47 -0600

    fixing final changes

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.get("", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+  app.use(express.static(path.join(__dirname, "public")));
 });
-app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(PORT, () => {
   console.log(`Current Mobile Offerwall Running on Port ${PORT}`);
