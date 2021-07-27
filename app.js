ff5404db9d2350dbe3b2f9731f7b9f54ab269e52
Titus . Roman <titus@CIT-SCC123-1524.cs.dixie.edu>
2021-07-27 11:04:18 -0600

    finally deployable

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(PORT, () => {
   console.log(`Current Mobile Offerwall Running on Port ${PORT}`);
