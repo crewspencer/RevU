addb445c1fa557fc3bacd1eaad93a6b75ffb2d74
Titus . Roman <titus@CIT-SCC123-1524.cs.dixie.edu>
2021-07-27 10:55:34 -0600

    adding fixes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3001;
 
-app.get("", (req, res) => {
-  res.sendFile("/index.html");
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(PORT, () => {
